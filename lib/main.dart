70f4141850750654644215edd3aa08fb81569b6c
Maxime <maxime.b2494@gmail.com>
2024-04-02 11:52:56 +0200

    widget column fini

--- lib/main.dart
+++ lib/main.dart
@@ -7,16 +7,31 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        decoration: BoxDecoration(
-          gradient: LinearGradient(colors: [Colors.blue, Colors.red]),
-          border: Border.all(color: Colors.teal, width: 3.00, style: BorderStyle.solid),
-          boxShadow: [BoxShadow(color: Colors.black, spreadRadius: 15.0, blurRadius: 10.0)],
-          // borderRadius: BorderRadius.circular(30)         
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      verticalDirection: VerticalDirection.down,
+      children: <Widget>[
+        Container(
+          color: Colors.blue,
+          height: 150,
         ),
-        child: Text('hello world', textDirection: TextDirection.ltr,)
-      )
+        Expanded(
+          child: Container(
+          color: Colors.red,
+          height: 150,
+          )
+        ),
+        Expanded(
+          child: Container(
+          color: Colors.green,
+          height: 150,
+          )
+        ),
+        Container(
+          color: Colors.purple,
+          height: 150,
+        ),
+      ],
     );
   }
 }
