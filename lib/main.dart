f9c0513ec306c2c6e6867eed015644c2e812eeae
Maxime <maxime.b2494@gmail.com>
2024-04-02 11:04:58 +0200

    widget container fini

--- lib/main.dart
+++ lib/main.dart
@@ -7,14 +7,24 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Image(
-          image: AssetImage(
-            'assets/bird.jpg'
-          ),
-          width: 300.0,
-          height: 600.0,
-        )
+    return Container(
+      color: Colors.green,
+      alignment: Alignment.center,
+      margin: EdgeInsets.all(20),
+      child: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(colors: [Colors.blue, Colors.red]),
+          border: Border.all(color: Colors.teal, width: 3.00, style: BorderStyle.solid),
+          boxShadow: [BoxShadow(color: Colors.black, spreadRadius: 15.0, blurRadius: 10.0)],
+          // borderRadius: BorderRadius.circular(30)
+          shape: BoxShape.circle
+        ),
+        width: 150.0,
+        height: 200.0,
+        padding: EdgeInsets.all(20.0),
+        alignment: Alignment.centerLeft,
+        child: Text('hello world !' , textDirection: TextDirection.ltr,)
+      )
     );
   }
 }
