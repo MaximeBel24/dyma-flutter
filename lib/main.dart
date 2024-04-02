bd80d21a560cade03fc5ae6938cc7d0939cff806
Maxime <maxime.b2494@gmail.com>
2024-04-02 17:48:23 +0200

    intro material fini

--- lib/main.dart
+++ lib/main.dart
@@ -7,42 +7,86 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-      verticalDirection: VerticalDirection.down,
-      children: <Widget>[
-        Container(
-          color: Colors.blue,
-          height: 150,
+    return MaterialApp(
+      title: 'homepage',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Homepage'),
+          backgroundColor: Colors.purple[200],
+          actions: <Widget>[
+            IconButton(
+              icon: Icon(Icons.sync),
+              onPressed: () => {print('click')},
+            )
+          ],
         ),
-        Expanded(
-          child: Container(
-            color: Colors.red,
-            height: 150,
-            child: Stack(
-              alignment: Alignment.center,
-              children: <Widget>[
-                Text('Hello world', textDirection: TextDirection.ltr),
-                Container(
-                  color: Colors.red.withOpacity(0.2),
-                  width: 300.00,
-                  height: 100.00,
-                ),                
-              ],
+        body: Container(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              TextButton(
+                onPressed: () => {}, 
+                child: Text('Je suis un bouton')
+              ),
+              ElevatedButton(
+                onPressed: () => {}, 
+                child: Text('Je suis un bouton',style: TextStyle(color: Colors.white),),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.purple[200]
+                ),
+              )
+            ]),
+          alignment: Alignment.center,
+        ),
+        bottomNavigationBar: BottomNavigationBar(
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.home),
+              label: 'home'
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.flight_land),
+              label: 'land'
+              ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.motorcycle),
+              label: 'moveTo'
             ),
+          ],
+        ),
+        drawer: Drawer(
+          child: Column(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.all(20.00),
+                child: Row(
+                  children: [
+                    Icon(Icons.supervised_user_circle),
+                    Padding(
+                      padding: EdgeInsets.only(left: 15.00),
+                      child: Text('profil')
+                    )
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Icon(Icons.surround_sound),
+                  Text('sound')
+                ],
+              )
+            ],
           )
         ),
-        Expanded(
-          child: Container(
-          color: Colors.green,
-          height: 150,
-          )
+        floatingActionButton: FloatingActionButton(
+          onPressed: () => {},
+          child: Icon(Icons.add)
         ),
-        Container(
-          color: Colors.purple,
-          height: 150,
-        ),
-      ],
+        floatingActionButtonLocation: FloatingActionButtonLocation.endFloat,
+      )
     );
   }
 }
