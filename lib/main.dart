4f60ed6612b2b2bdaf25fe015e0d8b4e6c71f996
Maxime <maxime.b2494@gmail.com>
2024-04-02 11:16:50 +0200

    widget padding fini

--- lib/main.dart
+++ lib/main.dart
@@ -7,23 +7,15 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.green,
-      alignment: Alignment.center,
-      margin: EdgeInsets.all(20),
+    return Center(
       child: Container(
         decoration: BoxDecoration(
           gradient: LinearGradient(colors: [Colors.blue, Colors.red]),
           border: Border.all(color: Colors.teal, width: 3.00, style: BorderStyle.solid),
           boxShadow: [BoxShadow(color: Colors.black, spreadRadius: 15.0, blurRadius: 10.0)],
-          // borderRadius: BorderRadius.circular(30)
-          shape: BoxShape.circle
+          // borderRadius: BorderRadius.circular(30)         
         ),
-        width: 150.0,
-        height: 200.0,
-        padding: EdgeInsets.all(20.0),
-        alignment: Alignment.centerLeft,
-        child: Text('hello world !' , textDirection: TextDirection.ltr,)
+        child: Text('hello world', textDirection: TextDirection.ltr,)
       )
     );
   }
